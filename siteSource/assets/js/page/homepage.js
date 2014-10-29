1918593d83346f3dae675fa1eecd5ec715cc530e
Guo Du <mrduguo@gmail.com>
2014-10-29 22:09:12 +0000

    homepage styling updated

---
+++ siteSource/assets/js/page/homepage.js
@@ -0,0 +1,26 @@
+$(function () {
+    var homepageTitleAnimation = $('#lifecycle .flat-title');
+    var resizeTitle = function () {
+        var movedPix = $(window).scrollTop() + 0;
+        if (movedPix > 200) {
+            movedPix = 200;
+        }
+        var newHeight = movedPix / 2 + 80
+        if (homepageTitleAnimation.css('height') != newHeight) {
+            homepageTitleAnimation.css({
+                height: newHeight,
+                top: -newHeight,
+                paddingTop: movedPix / 4 + 'px',
+                marginBottom: -newHeight,
+                backgroundColor: 'rgba(48,68,87,' + (0.8 + movedPix / 1000) + ')'
+            });
+        }
+    }
+
+    if (homepageTitleAnimation.length && homepageTitleAnimation.css('position') == 'relative'){
+        resizeTitle();
+        $(window).scroll(function () {
+            resizeTitle();
+        });
+    }
+});
